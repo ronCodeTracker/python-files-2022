5074406086ed21ee20fa99110079196dc274040c
unknown <2fixelectronics777@gmail.com>
2022-06-13 15:24:39 -0500

    add more python mod

---
+++ error_handle.py
@@ -0,0 +1,42 @@
+﻿# name:  Ronald Kiefer
+# hebrew:  ר ו נ  א ל ד
+# date: June 13, 2022 Monday
+# description:  list comprehensions bootcamp python
+
+
+class FileManipulator:
+  def __init__(self, fileName):
+    self.contents = None
+    while self.contents == None:
+      try:
+        myfile = open(fileName, 'r') # Open a file for 
+        self.contents = myfile.readlines() # Read in the content by line.
+      except (FileNotFoundError, TypeError, OSError) as e:
+        print(type(e), e)
+        fileName = input("Please enter a valid file name: ")
+      else:
+        myfile.close()
+      
+  def reverse(self,fileName):
+    while True:
+      try:
+        myfile = open(fileName, 'w') # Open a file for writing
+        revContent = [x.strip()[::-1] for x in self.contents]
+        for i in range(len(revContent)):
+          if i == (len(revContent)-1):
+            myfile.write(revContent[i])
+          else:
+            myfile.write(revContent[i] + '\n') # write desired output to file
+      except (FileNotFoundError, TypeError, OSError) as e:
+        print(type(e),e)
+        fileName = input("Please enter a valid file name: ")
+      else:
+        myfile.close()
+        break
+
+
+
+
+f = FileManipulator("tmp.txt")
+print(f.contents)
+f.reverse("tmp2.txt")
---
+++ lambda_functions.py
@@ -0,0 +1,34 @@
+﻿# name:  Ronald Kiefer
+# hebrew:  ר ו נ  א ל ד
+# date: June 13, 2022 Monday
+# description:  list comprehensions bootcamp python
+
+
+
+list_one = [('English', 88), ('Science', 90), 
+            ('Math', 97), ('Social Science', 82)]
+
+print_one = sorted(list_one, key =  lambda list_one: list_one[1])
+
+print(print_one)
+
+
+list = [3, 6, 9, 2]
+
+print_two = lambda list: [i**3 for i in list]  
+
+print(print_two(list))
+
+
+lambda_one = lambda num:True if  num % 2 == 0 else  False
+print([lambda_one(x) for x in [3, 6, 9, 2]])
+
+list_two = []
+[list_two.append(i) for i in range(1 , 101)]
+
+print(list_two)
+
+sent = "The quick brown fox jumped over the fence."
+print({word:len(word) for word in sent.split()})
+
+
